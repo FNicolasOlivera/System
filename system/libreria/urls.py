f6deea4dbe7c0a5cc5673aad8db8db84cd56b9be
NicolasOlivera <95304@sistemas.frc.utn.edu.ar>
2025-03-23 18:10:40 -0300

    23/03

--- system/libreria/urls.py
+++ system/libreria/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.inicio, name='inicio'),
-    path('nosotros', views.nosotros, name='nosotros')
+    path('nosotros', views.nosotros, name='nosotros'),
+    path('caballos', views.caballos, name='caballos'),
+    path('caballos/crear', views.crear, name='crear')
     ]
--- system/libreria/views.py
+++ system/libreria/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def inicio (request) :
-    return HttpResponse("<h1>Bienvenidos!</h1>")
+    return render(request, 'paginas/inicio.html')
 def nosotros(request):
     return render(request, 'paginas/nosotros.html')
+def caballos(request):
+    return render(request, 'entidades/index.html')
+def crear(request):
+    return render (request,'entidades/crear.html')
